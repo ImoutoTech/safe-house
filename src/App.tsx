619eedc502ee760528bda627d8d9e695b0bca19c
youranreus <youranreus@qq.com>
2023-03-16 23:13:31 +0800

    feat: Geist组件库引入

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import { Page } from "@geist-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <p>hello</p>
-    </div>
+    <Page>
+      <Page.Header>
+        <h2>头部</h2>
+      </Page.Header>
+      <Page.Content>
+        <Outlet />
+      </Page.Content>
+      <Page.Footer>
+        <h2>尾部</h2>
+      </Page.Footer>
+    </Page>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { GeistProvider, CssBaseline } from "@geist-ui/core";
 
 import router from "./router/router";
 
@@ -8,6 +9,9 @@ import "normalize.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <GeistProvider>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </GeistProvider>
   </React.StrictMode>
 );
