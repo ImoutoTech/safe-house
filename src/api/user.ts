bdd74a7fd0e767765b95f9482e42c591f47a97f4
季悠然 <youranreus@qq.com>
2024-04-16 19:10:59 +0800

    feat: user api完善

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,4 +1,13 @@
-import type { Restful, UserInfo } from '@/types'
+import type {
+  LoginResult,
+  Pagination,
+  Restful,
+  UserAvatarModifyParams,
+  UserInfo,
+  UserLoginParams,
+  UserModifyParams,
+  UserRegisterParams
+} from '@/types'
 import api from './api'
 
 export const refreshToken = () => {
@@ -11,3 +20,17 @@ export const refreshToken = () => {
 }
 
 export const getUserData = (id: number) => api.Get<Restful<UserInfo>>(`/user/${id}`)
+
+export const userLogin = (data: UserLoginParams, md5 = true) =>
+  api.Post<Restful<LoginResult>>(`/user/login?md5=${md5}`, data)
+
+export const userRegister = (data: UserRegisterParams, md5 = true) =>
+  api.Post<Restful<UserInfo>>(`/user/register?md5=${md5}`, data)
+
+export const updateUserData = (id: number, data: UserModifyParams | UserAvatarModifyParams) =>
+  api.Put<Restful<UserInfo>>(`/user/${id}`, data)
+
+export const getAllUser = (page = 1, size = 10, search = '') =>
+  api.Get<Restful<Pagination<UserInfo>>>(`/user/all`, {
+    params: { page, size, search }
+  })
