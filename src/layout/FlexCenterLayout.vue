10673486f65ad9f0d2cea17f1f4435c2a091d68d
季悠然 <youranreus@qq.com>
2024-04-17 01:07:26 +0800

    feat: FlexCenterLayout

---
+++ src/layout/FlexCenterLayout.vue
@@ -0,0 +1,26 @@
+<template>
+  <n-flex class="flex-center-layout" justify="center" align="center">
+    <slot v-if="type === 'slot'" />
+    <router-view v-if="type === 'router'"></router-view>
+  </n-flex>
+</template>
+<script setup lang="ts">
+defineOptions({
+  name: 'FlexCenterLayout'
+})
+
+withDefaults(
+  defineProps<{
+    type?: 'router' | 'slot'
+  }>(),
+  {
+    type: 'slot'
+  }
+)
+</script>
+<style lang="scss" scoped>
+.flex-center-layout {
+  width: 100%;
+  height: 100%;
+}
+</style>
