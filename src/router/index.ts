51a5354892a1270815f898ca59ce429584810a79
季悠然 <youranreus@qq.com>
2024-04-17 17:13:15 +0800

    feat: 路由权限校验

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import BaseLayout from '@/layout/BaseLayout.vue'
+import { UserRole } from '@reus-able/types'
+import { useUserStore } from '@/stores/user'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,10 +26,36 @@ const router = createRouter({
           path: 'register',
           name: 'register',
           component: () => import('../views/user/register-index.vue')
+        },
+        {
+          path: 'user',
+          name: 'user-layout',
+          component: () => import('../views/user/view-index.vue'),
+          meta: {
+            role: UserRole.USER
+          }
         }
       ]
     }
   ]
+})
+
+router.beforeEach((to) => {
+  const { hasLogin, userData } = useUserStore()
+  console.log(to.matched)
+
+  for (const route of [...to.matched].reverse()) {
+    if (route.meta.role) {
+      const authMap = {
+        [UserRole.ADMIN]: hasLogin.value && userData.value.role === UserRole.ADMIN,
+        [UserRole.USER]: hasLogin.value
+      }
+
+      return authMap[route.meta.role as UserRole] || { name: 'login' }
+    }
+  }
+
+  return true
 })
 
 export default router
---
+++ src/views/user/view-index.vue
@@ -0,0 +1,12 @@
+<template>
+  <flex-center-layout>
+    <div>user-index</div>
+  </flex-center-layout>
+</template>
+<script setup lang="ts">
+import FlexCenterLayout from '@/layout/FlexCenterLayout.vue'
+
+defineOptions({
+  name: 'UserIndex'
+})
+</script>
