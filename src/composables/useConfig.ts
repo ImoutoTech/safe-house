a7d70e53e696fe97cb20e16144ba2bf12b65095f
季悠然 <youranreus@qq.com>
2024-04-17 16:55:15 +0800

    feat: 优化useConfig

--- src/composables/useConfig.ts
+++ src/composables/useConfig.ts
@@ -1,12 +1,13 @@
 import { getDynamicConfig } from '@/api/config'
 import { type Config, useConfigStore } from '@/stores/config'
 import { useRequest } from 'alova'
+import { isNil } from 'lodash-es'
 
 export const useConfig = (needRefresh = false) => {
-  const { loading, onSuccess } = useRequest(getDynamicConfig<Config>('safe-house-index'), {
-    immediate: needRefresh
-  })
   const { config, updateConfig } = useConfigStore()
+  const { loading, onSuccess } = useRequest(getDynamicConfig<Config>('safe-house-index'), {
+    immediate: needRefresh || isNil(config.value)
+  })
 
   onSuccess((res) => {
     updateConfig(res.data)
